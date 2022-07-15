21ca0c50e2d5b73c8f0f277e64c21ed83ea6d0af
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-15 15:46:24 -0300

    create table report

---
+++ server/db/schema/01_create_report.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS report CASCADE;
+
+CREATE TABLE report (
+  id SERIAL PRIMARY KEY,
+  doctor_name VARCHAR(255),
+  patient_name VARCHAR(255),
+  subject VARCHAR(255),
+  status VARCHAR(255),
+  contact_date DATE,
+  contact_tool VARCHAR(255)
+);
--- server/db/schema/01_create_users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE test (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  teste INTEGER
-);
