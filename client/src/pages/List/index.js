cda496e3e18d2062f67c5621ba7930b85164e417
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-15 19:41:26 -0300

    fetching data from api in List

--- client/src/pages/List/index.js
+++ client/src/pages/List/index.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 function List() {
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios.get('api/report')
+    .then(res => {
+      setList(res.data)
+      console.log(res.data) 
+    })
+    
+  }, [])
+
   return(
     <div>
-      <p>lista</p>
+      {list.map(item => {
+        return (
+          <p>{item.doctor_name}</p>
+        )
+      })}
     </div>
   )
 }
--- server/routes/report.js
+++ server/routes/report.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 module.exports = (db) => {
   router.get('/', (req, res) => {
     db.query(`
-      select * from report;
+      SELECT *
+      FROM report
+      ORDER BY id DESC;
     `)
     .then(data => {
       res.json(data.rows);
