99d858a3f2bccec9469292d862aa87fc0f60b2e7
gabrielcelligoi <gabrielcelligoi@gmail.com>
2022-07-18 17:16:49 -0300

    start form on register page

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,7 +1,25 @@
+import './register.css';
+
 function Register() {
   return(
     <div>
       <p>Registre uma entrada</p>
+      <form>
+        <label htmlFor='doctor_name'>Doctor's Name </label>
+        <input type="text" id="doctor_name"></input>
+
+        <label htmlFor='patient_name'>Patient's Name </label>
+        <input type="text" id="patient_name"></input>
+
+        <label htmlFor='subject'>Subject </label>
+        <input type="text" id="subject"></input>
+
+        <label htmlFor='status'>Status </label>
+        <input type="text" id="status"></input>
+
+        
+
+      </form>
     </div>
   )
 }
